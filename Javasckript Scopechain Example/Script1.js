705b109ceadef44ef276581fe5a9bee3b827c14f
htmlcssjavascriptcoursera <h1609397@mvrht.com>
2017-06-09 07:26:40 +0200

    Scopechain examples.

--- Javasckript Scopechain Example/Script1.js
+++ Javasckript Scopechain Example/Script1.js
@@ -1,5 +1,31 @@
-// JavaScript source code
+/*  
+    x=2 will be desplayed.
+    Because function B is defined in the Global Scope and not within A and thus,
+    B's reference to outer environment is reference to Global Scope and not scope of function A,
+    hence x from Global Scobe is desplayed and not x from A's scope.
+
+    Same as with variable message.
+*/
 var x = 2;
+var message = "in global";
+
+console.log("global: message = " + message);
+
+function TextB() {
+    console.log("b (B is defined inside Global Scope.): message = " + message);
+}
+
+function TextA() {
+    var message = "inside a";
+    console.log("a: message = " + message);
+
+    function TexTC() {
+        console.log("c (C is defined inside A.): message = " + message);
+    }
+
+    TextB();
+    TexTC();
+}
 
 function B() {
     console.log(x);
@@ -10,11 +36,5 @@ function A() {
     B();
 }
 
+TextA();
 A();
-
-/*  
-    x=2 will be desplayed.
-    Because function B is defined in the Global Scope and not within A and thus,
-    B's reference to outer environment is reference to Global Scope and not scope of function A,
-    hence x from Global Scobe is desplayed and not x from A's scope.
-*/
